8519a31f9b12f8c775a5a4afcbcb9175e6035990
Nguyen Hung Cuong <cuonghungnguyentop@gmail.com>
2026-03-29 18:19:54 +0700

    Thiết lập Eureka Server, API Gateway và tạo routing tới Identity Service

---
+++ src/api-gateway/src/test/java/com/seika/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seika.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
