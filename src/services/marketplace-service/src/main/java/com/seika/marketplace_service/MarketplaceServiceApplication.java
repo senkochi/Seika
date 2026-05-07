cdb88fa27a112677ae9097559361851a761d7d65
NguyenHungCuongg <cuonghungnguyentop@gmail.com>
2026-05-07 10:24:16 +0700

    feat: khởi tạo marketplace service

---
+++ src/services/marketplace-service/src/main/java/com/seika/marketplace_service/MarketplaceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.seika.marketplace_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketplaceServiceApplication.class, args);
+	}
+
+}
