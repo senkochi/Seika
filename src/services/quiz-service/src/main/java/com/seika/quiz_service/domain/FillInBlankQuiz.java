d4a0d2e9f35a131364136ff9e2b42b17abb0605b
senkochi <senkosannanoja@gmail.com>
2026-05-10 00:40:27 +0700

    feat: create quiz api and create quiz request dtos

--- src/services/quiz-service/src/main/java/com/seika/quiz_service/domain/FillInBlankQuiz.java
+++ src/services/quiz-service/src/main/java/com/seika/quiz_service/domain/FillInBlankQuiz.java
@@ -1,14 +1,15 @@
 package com.seika.quiz_service.domain;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.seika.quiz_service.constant.enums.QuizType;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.TypeAlias;
 
 import java.util.List;
 
 @Data
 @TypeAlias("FILL_BLANK")
+@JsonTypeName("FILL_IN_BLANK")
 public class FillInBlankQuiz extends BaseQuiz {
     // Chấp nhận danh sách các đáp án (đề phòng đồng nghĩa)
     private List<String> acceptedAnswers;
--- src/services/quiz-service/src/main/java/com/seika/quiz_service/domain/MatchingQuiz.java
+++ src/services/quiz-service/src/main/java/com/seika/quiz_service/domain/MatchingQuiz.java
@@ -1,14 +1,15 @@
 package com.seika.quiz_service.domain;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.seika.quiz_service.constant.enums.QuizType;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.TypeAlias;
 
 import java.util.Map;
 
 @Data
 @TypeAlias("MATCHING")
+@JsonTypeName("MATCHING")
 public class MatchingQuiz extends BaseQuiz {
     private Map<String, String> matchingPairs;
 
