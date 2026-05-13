de7e1c459dab85df522aab37cffdcbdb6462ff09
NguyenHungCuongg <cuonghungnguyentop@gmail.com>
2026-05-13 14:40:54 +0700

    feat: update MongoDB configuration for flashcard, notification, and quiz services

--- src/services/quiz-service/src/main/java/com/seika/quiz_service/config/MongoConfig.java
+++ src/services/quiz-service/src/main/java/com/seika/quiz_service/config/MongoConfig.java
@@ -1,4 +1,4 @@
-package com.seika.flashcard_service.config;
+package com.seika.quiz_service.config;
 
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
@@ -10,36 +10,31 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 import java.util.Objects;
-import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class MongoConfig {
 
-    @Value("${spring.data.mongodb.uri:${FLASHCARD_DB_URI}}")
+    @Value("${spring.data.mongodb.uri:${QUIZ_DB_URI}}")
     private String mongoUri;
 
-    @Value("${spring.data.mongodb.database:${FLASHCARD_DB_DATABASE}}")
+    @Value("${spring.data.mongodb.database:${QUIZ_DB_DATABASE}}")
     private String databaseName;
 
     @Bean
-    public MongoClient mongoClient(){
+    public MongoClient mongoClient() {
         MongoClientSettings settings = MongoClientSettings.builder()
                 .applyConnectionString(new ConnectionString(mongoUri))
-                .applyToSocketSettings(builder ->
-                        builder.connectTimeout(30, TimeUnit.SECONDS)
-                                .readTimeout(30, TimeUnit.SECONDS))
                 .build();
 
         return MongoClients.create(settings);
     }
 
     @Bean
-    public MongoTemplate mongoTemplate(){
+    public MongoTemplate mongoTemplate() {
         String resolvedDatabase = Objects.requireNonNullElseGet(
                 new ConnectionString(mongoUri).getDatabase(),
                 () -> databaseName);
 
         return new MongoTemplate(mongoClient(), resolvedDatabase);
     }
-
 }
