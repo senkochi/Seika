271740f1d542ace3dbca8e6fd8ca27ad46d73402
NguyenHungCuongg <cuonghungnguyentop@gmail.com>
2026-06-04 16:39:43 +0700

    feat: add Game Profile

--- src/services/profile-service/src/main/java/com/seika/profile_service/mapper/UserProfileMapper.java
+++ src/services/profile-service/src/main/java/com/seika/profile_service/mapper/UserProfileMapper.java
@@ -2,13 +2,16 @@ package com.seika.profile_service.mapper;
 
 import com.seika.profile_service.dto.user_profile.UserProfileResponse;
 import com.seika.profile_service.enity.UserProfile;
+import com.seika.profile_service.enity.GameProfile;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface UserProfileMapper {
-    UserProfileResponse toUserProfileResponse(UserProfile userProfile);
+    @Mapping(target="userId", source = "userProfile.userId")
+    UserProfileResponse toUserProfileResponse(UserProfile userProfile, GameProfile gameProfile);
 
     List<UserProfileResponse> toUserProfileResponseList(List<UserProfile> userProfileList);
 }
