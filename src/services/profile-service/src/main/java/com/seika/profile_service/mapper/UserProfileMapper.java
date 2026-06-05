5aa9d651fb3c6d07200f76279d91102cfca4a0b3
NguyenHungCuongg <cuonghungnguyentop@gmail.com>
2026-06-05 11:22:15 +0700

    fix: fix user profile mapper

--- src/services/profile-service/src/main/java/com/seika/profile_service/mapper/UserProfileMapper.java
+++ src/services/profile-service/src/main/java/com/seika/profile_service/mapper/UserProfileMapper.java
@@ -1,6 +1,7 @@
 package com.seika.profile_service.mapper;
 
 import com.seika.profile_service.dto.user_profile.UserProfileResponse;
+import com.seika.profile_service.enity.GameProfile;
 import com.seika.profile_service.enity.UserProfile;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -10,7 +11,7 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface UserProfileMapper {
     @Mapping(target = "userId", source = "userProfile.userId")
-    UserProfileResponse toUserProfileResponse(UserProfile userProfile);
+    UserProfileResponse toUserProfileResponse(UserProfile userProfile, GameProfile gameProfile);
 
     List<UserProfileResponse> toUserProfileResponseList(List<UserProfile> userProfileList);
 }
